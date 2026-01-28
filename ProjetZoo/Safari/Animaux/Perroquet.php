936ef635bd43c717b1d52137b6d33d38413ad511
Lukas <lukas.boudinl@gmail.com>
2026-01-28 14:48:25 +0100

    complétion de la class perroquet

--- ProjetZoo/Safari/Animaux/Perroquet.php
+++ ProjetZoo/Safari/Animaux/Perroquet.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace Safari\Animal;
+
+class Perroquet extends Animal{
+    public function crier(){
+        return "Coco veut un gâteau ! (".$this->getNom()." siffle)";
+    }
+
+    public function voler(){
+        return "le perroquet s'envole";
+    }
+}
