c5d116967fb8a3519a03d20843ffdac9f65fc07c
Lukas <lukas.boudinl@gmail.com>
2026-01-28 14:37:04 +0100

    complétion de la class Animal

--- ProjetZoo/Safari/Animaux/Animal.php
+++ ProjetZoo/Safari/Animaux/Animal.php
@@ -1,1 +1,37 @@
 <?php
+namespace Safari\Animal;
+class Animal {
+    protected string $nom;
+    protected int $poids;
+    protected bool $estMalade = false;
+
+    public function __construct($nom, $poids){
+        $this->nom = $nom;
+        $this->poids = $poids;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+    public function getPoids() {
+        return $this->poids;
+    }
+    public function getEstMalade() {
+        return $this->estMalade;
+    }
+    public function setEstMalade() {
+        $this->estMalade = true;
+    }
+
+    public function manger(){
+        return "L'animal mange";
+    }
+
+    public function  seSoigner(){
+        $this->estMalade = false;
+    }
+
+    public function crier(){
+        return "GRR";
+    }
+}
